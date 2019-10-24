82ef65a9ba83e98be5375ae7d55e45b431feb34f
Juan Solo <juan.marinan.c@gmail.com>
2019-10-24 00:56:26 -0300

    Se agrega pagina diccionario

---
+++ src/app/diccionario/diccionario.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diccionario',
+  templateUrl: './diccionario.page.html'
+})
+export class DiccionarioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
